832ebdf0c079dd2a64868f5c5bd67887ac2e2bf3
babak_keshavarz <babak0keshavarz@gmail.com>
2025-09-19 17:15:48 -0700

    completed content based recommender

--- imdb_recommender.py
+++ imdb_recommender.py
@@ -89,3 +89,109 @@ def get_recommendations(title, cosine_sim=cosine_sim):
 
 movie_name = 'The Dark Knight Rises'
 print(f"Recommendations for {movie_name} are: {get_recommendations(movie_name)}")
+
+
+
+
+#### adding more meta data
+# Load keywords and credits
+credits = pd.read_csv('data/credits.csv')
+keywords = pd.read_csv('data/keywords.csv')
+
+# Remove rows with bad IDs.
+metadata = metadata.drop([19730, 29503, 35587])
+
+# Convert IDs to int. Required for merging
+keywords['id'] = keywords['id'].astype('int')
+credits['id'] = credits['id'].astype('int')
+metadata['id'] = metadata['id'].astype('int')
+
+# Merge keywords and credits into your main metadata dataframe
+metadata = metadata.merge(credits, on='id')
+metadata = metadata.merge(keywords, on='id')
+
+# Parse the stringified features into their corresponding python objects
+from ast import literal_eval
+
+features = ['cast', 'crew', 'keywords', 'genres']
+for feature in features:
+    metadata[feature] = metadata[feature].apply(literal_eval)
+
+
+import numpy as np
+
+def get_director(x):
+    for i in x:
+        if i['job'] == 'Director':
+            return i['name']
+    return np.nan
+
+
+def get_list(x):
+    if isinstance(x, list):
+        names = [i['name'] for i in x]
+        #Check if more than 3 elements exist. If yes, return only first three. If no, return entire list.
+        if len(names) > 3:
+            names = names[:3]
+        return names
+
+    #Return empty list in case of missing/malformed data
+    return []
+
+
+# Define new director, cast, genres and keywords features that are in a suitable form.
+metadata['director'] = metadata['crew'].apply(get_director)
+
+features = ['cast', 'keywords', 'genres']
+for feature in features:
+    metadata[feature] = metadata[feature].apply(get_list)
+
+
+# Print the new features of the first 3 films
+metadata[['title', 'cast', 'director', 'keywords', 'genres']].head(3)
+
+
+# Function to convert all strings to lower case and strip names of spaces
+def clean_data(x):
+    if isinstance(x, list):
+        return [str.lower(i.replace(" ", "")) for i in x]
+    else:
+        #Check if director exists. If not, return empty string
+        if isinstance(x, str):
+            return str.lower(x.replace(" ", ""))
+        else:
+            return ''
+
+
+# Apply clean_data function to your features.
+features = ['cast', 'keywords', 'director', 'genres']
+
+for feature in features:
+    metadata[feature] = metadata[feature].apply(clean_data)
+
+def create_soup(x):
+    return ' '.join(x['keywords']) + ' ' + ' '.join(x['cast']) + ' ' + x['director'] + ' ' + ' '.join(x['genres'])
+
+# Create a new soup feature
+metadata['soup'] = metadata.apply(create_soup, axis=1)
+
+
+# Import CountVectorizer and create the count matrix
+from sklearn.feature_extraction.text import CountVectorizer
+
+count = CountVectorizer(stop_words='english')
+count_matrix = count.fit_transform(metadata['soup'])
+
+
+# Compute the Cosine Similarity matrix based on the count_matrix
+from sklearn.metrics.pairwise import cosine_similarity
+
+cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
+
+
+# Reset index of your main DataFrame and construct reverse mapping as before
+metadata = metadata.reset_index()
+indices = pd.Series(metadata.index, index=metadata['title'])
+
+
+print(get_recommendations('The Dark Knight Rises', cosine_sim2))
